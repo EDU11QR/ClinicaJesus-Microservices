42fb4bb688718d027a18c1b2472f59c6235c5338
EDU11QR <quisperojasedu@gmail.com>
2026-05-24 23:40:17 -0500

    Implementación de API Gateway y Auth Service con Eureka y login funcional

--- auth-service/src/main/java/com/clinicajesus/service/impl/UsuarioServiceImpl.java
+++ auth-service/src/main/java/com/clinicajesus/service/impl/UsuarioServiceImpl.java
@@ -6,7 +6,6 @@ import com.clinicajesus.entity.UsuarioEntity;
 import com.clinicajesus.repository.UsuarioRepository;
 import com.clinicajesus.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,10 +13,10 @@ import org.springframework.stereotype.Service;
 public class UsuarioServiceImpl implements UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public LoginResponse login(LoginRequest request){
+
         UsuarioEntity usuario = usuarioRepository
                 .findByUsernameOrEmail(
                         request.usernameOrEmail(),
@@ -25,12 +24,7 @@ public class UsuarioServiceImpl implements UsuarioService {
                 )
                 .orElseThrow(() -> new RuntimeException("Credenciales invalidas"));
 
-        boolean passwordOk = passwordEncoder.matches(
-                request.password(),
-                usuario.getPassword()
-        );
-
-        if (!passwordOk){
+        if (!request.password().equals(usuario.getPassword())) {
             throw new RuntimeException("Credenciales invalidas");
         }
 
@@ -43,7 +37,5 @@ public class UsuarioServiceImpl implements UsuarioService {
                 usuario.getTelefono(),
                 usuario.getActivo()
         );
-
     }
-
 }
