70736ed014b51290d1a744e6ac77d45e696f3bd0
EDU11QR <quisperojasedu@gmail.com>
2026-06-13 17:01:05 -0500

    Migrar entidad cita y estado a appointment-service

---
+++ appointment-service/src/main/java/com/clinicajesus/entity/CitaEntity.java
@@ -0,0 +1,64 @@
+package com.clinicajesus.entity;
+
+import com.clinicajesus.enums.EstadoCita;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "citas")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CitaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long pacienteId;
+
+    private Long doctorId;
+
+    private Long horarioDisponibleId;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    @Column(length = 3, nullable = false)
+    private String moneda;
+
+    @Column(nullable = false, length = 500)
+    private String motivoConsulta;
+
+    @Column(length = 1000)
+    private String observaciones;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EstadoCita estado;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaHoraCreacion;
+
+    @PrePersist
+    public void prePersist() {
+
+        if (estado == null) {
+            estado = EstadoCita.PENDIENTE;
+        }
+
+        if (fechaHoraCreacion == null) {
+            fechaHoraCreacion = LocalDateTime.now();
+        }
+
+        if (moneda == null) {
+            moneda = "PEN";
+        }
+    }
+}
---
+++ appointment-service/src/main/java/com/clinicajesus/entity/enums/EstadoCita.java
@@ -0,0 +1,10 @@
+package com.clinicajesus.enums;
+
+public enum EstadoCita {
+
+    PENDIENTE,
+    CONFIRMADA,
+    CANCELADA
+
+}
+
