0a1886aa1b6f957a4133568f1416258bdc38af54
EDU11QR <quisperojasedu@gmail.com>
2026-06-15 13:51:54 -0500

    feat: registrar payment service en eureka server

---
+++ payment-service/src/test/java/com/clinicajesus/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicajesus.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
